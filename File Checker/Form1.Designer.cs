ab5fe37e80473494da011011227e8f66cf190bc0
Matt Howard <matthoward30740@gmail.com>
2022-06-11 17:38:20 -0400

    Add project files.

---
+++ File Checker/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace File_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bw = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Main";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Backup";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 68);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(313, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 127);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(313, 201);
+            this.textBox3.TabIndex = 7;
+            // 
+            // bw
+            // 
+            this.bw.WorkerReportsProgress = true;
+            this.bw.WorkerSupportsCancellation = true;
+            this.bw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_DoWork);
+            this.bw.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bw_ProgressChanged);
+            this.bw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 340);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "File Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.ComponentModel.BackgroundWorker bw;
+    }
+}
+
---
+++ File Checker/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Deployment.Application;
+
+namespace File_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                this.Text += " (" + ApplicationDeployment.CurrentDeployment.CurrentVersion + ")";
+            }
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bw.ReportProgress(0, "Starting...\r\n");
+
+            System.IO.DirectoryInfo dir1 = new System.IO.DirectoryInfo(textBox1.Text);
+            System.IO.DirectoryInfo dir2 = new System.IO.DirectoryInfo(textBox2.Text);
+            bw.ReportProgress(20, "Capturing Main Directories...\r\n");
+            IEnumerable<System.IO.FileInfo> list1 = dir1.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            bw.ReportProgress(40, "Capturing Backup Directories...\r\n");
+            IEnumerable<System.IO.FileInfo> list2 = dir2.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+            FileCompare myFileCompare = new FileCompare();
+
+            
+            if ((bw.CancellationPending == true))
+            {
+                e.Cancel = true;
+
+            }
+            else
+            {
+                
+                var queryList1Only = (from file in list1
+                                      select file).Except(list2, myFileCompare);
+                bw.ReportProgress(60, "Comparing Directories...\r\n");
+                bw.ReportProgress(80, "Preparing List...\r\n");
+                foreach (var v in queryList1Only)
+                {
+                    
+                    string badFile = v.FullName.ToString() + "\r\n";
+                    if (!badFile.Contains("._"))
+                    {
+                        
+                        bw.ReportProgress(80, badFile + "\r\n");
+                    }
+
+                }
+                bw.ReportProgress(100, "Finishing...\r\n");
+                
+            }
+        }
+
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Change the value of the ProgressBar to the BackgroundWorker progress.
+            progressBar1.Value = e.ProgressPercentage;
+            // Set the text.
+            //this.Text = e.ProgressPercentage.ToString();
+            //this.textBox3.Text = (e.ProgressPercentage.ToString() + "%");
+            this.textBox3.Text += (e.UserState.ToString());
+            this.textBox3.ScrollToCaret();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bw.IsBusy != true)
+            {
+                bw.RunWorkerAsync();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bw.WorkerSupportsCancellation == true)
+            {
+                bw.CancelAsync();
+            }
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                this.textBox3.Text += "Canceled!\r\n";
+                this.textBox3.ScrollToCaret();
+            }
+
+            else if (!(e.Error == null))
+            {
+                this.textBox3.Text += ("Error: " + e.Error.Message + "\r\n");
+                this.textBox3.ScrollToCaret();
+            }
+
+            else
+            {
+                this.textBox3.Text += "Done!\r\n";
+                this.textBox3.ScrollToCaret();
+            }
+        }
+
+       }
+
+
+    class FileCompare : System.Collections.Generic.IEqualityComparer<System.IO.FileInfo>
+    {
+        public FileCompare() { }
+
+        public bool Equals(System.IO.FileInfo f1, System.IO.FileInfo f2)
+        {
+            return (f1.Name == f2.Name &&
+                    f1.Length == f2.Length);
+        }
+
+        // Return a hash that reflects the comparison criteria. According to the  
+        // rules for IEqualityComparer<T>, if Equals is true, then the hash codes must 
+        // also be equal. Because equality as defined here is a simple value equality, not 
+        // reference identity, it is possible that two or more objects will produce the same 
+        // hash code. 
+        public int GetHashCode(System.IO.FileInfo fi)
+         {
+           string s = String.Format("{0}{1}", fi.Name, fi.Length);
+            return s.GetHashCode();
+        }
+    }
+}
+
